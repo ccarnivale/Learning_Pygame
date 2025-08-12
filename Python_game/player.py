12396bc06615f02f98b636e873d0c7d6e74cc1ff
Christopher Carnivale <tud11809@temple.edu>
2025-08-12 12:30:36 -0400

    Start to change to be os independent by os.join

--- Python_game/player.py
+++ Python_game/player.py
@@ -1,6 +1,7 @@
 import pygame
 from settings import *
 from support import *
+from os.path import join
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, group):
@@ -36,8 +37,13 @@ class Player(pygame.sprite.Sprite):
         self.animations = {'up': [], 'down':[], 'left': [], 'right': []} 
         #Fixed the file path issues. Directs to correct folder and uses the import_folder function to get all images in the folder.
         for animation in self.animations.keys():
-            full_path = "Assets\\Sprout Lands Sprites\\Sprout Lands - Sprites - Basic pack\\Characters\\" + animation 
-            self.animations[animation] = import_folder(full_path)   
+            full_path = join('Assets','Sprout Lands Sprites','Sprout Lands - Sprites - Basic pack','Characters') + animation 
+            self.animations[animation] = import_folder(full_path)
+    
+    #def import_tool_assets(self):
+    #    self.tool_animations = {'axe': [], 'hoe': [], 'water': []}
+    #    full_path = "Assets\\Sprout Lands Sprites\\Sprout Lands - Sprites - Basic pack\\Tools\\Axe"
+    #    self.tool_animations['axe'] = import_folder(full_path)  
     
     def animate(self, dt):
         if self.idle == False:
--- Python_game/tests.py
+++ Python_game/tests.py
@@ -1,7 +1,10 @@
 import os
+from os.path import join
 
-for folder in os.listdir("C:\\Users\\Chris Carnivale\\OneDrive\\Desktop\\Python_fun_project\\First-Python-Game\\Python_game\\Assets\\Sprout Lands Sprites\\Sprout Lands - Sprites - Basic pack\\Characters\\"):
-        print(folder)
+##for folder in os.listdir("C:\\Users\\Chris Carnivale\\OneDrive\\Desktop\\Python_fun_project\\First-Python-Game\\Python_game\\Assets\\Sprout Lands Sprites\\Sprout Lands - Sprites - Basic pack\\Characters\\"):
+#        print(folder)
 
-for folder in os.listdir("C:\\Users\\Chris Carnivale\\OneDrive\\Desktop\\Python_fun_project\\Python_game\\Assets\\Sprout Lands Sprites \\Sprout Lands - Sprites - Basic pack\\Characters\\"):
-        print(folder)
+#for folder in os.listdir("C:\\Users\\Chris Carnivale\\OneDrive\\Desktop\\Python_fun_project\\Python_game\\Assets\\Sprout Lands Sprites \\Sprout Lands - Sprites - Basic pack\\Characters\\"):
+#        print(folder)
+
+print(join('Assets','Sprout Lands Sprites','Sprout Lands - Sprites - Basic pack','Characters','down'))
